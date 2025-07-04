3c1d89f384e08ccc6f14051d0ec775a18ddec635
Rugvedrc <144464437+Rugvedrc@users.noreply.github.com>
2025-07-05 00:00:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -273,4 +273,6 @@ def handle_disconnect():
 def handle_file_created(data):
     pass
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
